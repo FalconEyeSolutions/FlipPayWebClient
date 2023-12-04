7b0fc5147d5e9b22ea321ee874e2b0eb18e60b1a
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2023-12-05 01:43:51 +1100

    Refactor: Correct GetProductsResponse
    Allows the GetProductsResponse to handle mixed-type arrays in JSON.

--- Models/General/GetProductsResponse.cs
+++ Models/General/GetProductsResponse.cs
@@ -2,13 +2,18 @@
 
 namespace FlipPayApiLibrary.Models.General;
 
+public interface IProductInfo{ }
+
 public record GetProductsResponse(
-    [property: JsonPropertyName("products")] List<ProductDetails>? Products
+    [property: JsonPropertyName("products")] List<IProductInfo>? Products
 );
 
 public record ProductDetails(
     [property: JsonPropertyName("productId")] string ProductId,
     [property: JsonPropertyName("minAmount")] decimal MinAmount,
-    [property: JsonPropertyName("maxAmount")] decimal MaxAmount,
-    [property: JsonPropertyName("merchantFacility")] bool? MerchantFacility
-);
+    [property: JsonPropertyName("maxAmount")] decimal MaxAmount
+) : IProductInfo;
+
+public record MerchantFacilityInfo(
+    [property: JsonPropertyName("merchantFacility")] bool MerchantFacility
+) : IProductInfo;
