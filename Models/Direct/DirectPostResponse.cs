24829b924c5ee34f793e78d7f49566b42daf5c69
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2023-12-05 18:06:56 +1100

    Enhance: Add Repayment Details to DirectPostResponse Model
    - Updated DirectPostResponse to include new 'message' and 'repayment' fields.
    - Created RepaymentDetails record to capture repayment information.
    - Created RepaymentOption record for detailed repayment options.
    - Adjusted JSON property names to match the updated API response structure.

--- Models/Direct/DirectPostResponse.cs
+++ Models/Direct/DirectPostResponse.cs
@@ -5,5 +5,23 @@ namespace FlipPayApiLibrary.Models.Direct;
 public record DirectPostResponse(
     [property: JsonPropertyName("prId")] string? PrId,
     [property: JsonPropertyName("prUrl")] string? PrUrl,
-    [property: JsonPropertyName("status")] string? Status
+    [property: JsonPropertyName("status")] string? Status,
+    [property: JsonPropertyName("message1")] string? MessageOne,
+    [property: JsonPropertyName("message2")] string? MessageTwo,
+    [property: JsonPropertyName("repayment")] RepaymentDetails? Repayment
+);
+
+public record RepaymentDetails(
+    [property: JsonPropertyName("reference")] string? Reference,
+    [property: JsonPropertyName("contactEmail")] string? ContactEmail,
+    [property: JsonPropertyName("contactPhone")] string? ContactPhone,
+    [property: JsonPropertyName("options")] List<RepaymentOption>? Options
+);
+
+public record RepaymentOption(
+    [property: JsonPropertyName("type")] string? Type,
+    [property: JsonPropertyName("accountName")] string? AccountName,
+    [property: JsonPropertyName("accountBsb")] string? AccountBsb,
+    [property: JsonPropertyName("accountNumber")] string? AccountNumber,
+    [property: JsonPropertyName("payID")] string? PayId
 );
